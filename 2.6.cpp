d8193531b78ea32bd8828bfd8441029c0ca78a58
misha1350 <74230991+misha1350@users.noreply.github.com>
2020-11-14 01:39:15 +0500

    Update 2.6.cpp

--- 2.6.cpp
+++ 2.6.cpp
@@ -5,17 +5,17 @@ int n;
 
 int main()
 {
-	cin >> n;
-	for (int i = n; i > 0; i--)
-	{
-		for (int j = i; j < n; j++)
-		{
-			cout << " ";
-		}
-		for (int j = 0; j < i; j++)
-		{
-			cout << "*";
-		}
-		cout << "\n";
-	}
+   cin >> n;
+   for (int i=n; i>0; i--)
+   {
+      for (int j=i; j<n; j++)
+      {
+         cout << " ";
+      }
+      for (int j=0; j<i; j++)
+      {
+         cout << "*";
+      }
+      cout << "\n";
+   }
 }
