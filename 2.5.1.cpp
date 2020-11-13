a5b79b3db75b4a462875de0cb942b986ae7d0d25
misha1350 <74230991+misha1350@users.noreply.github.com>
2020-11-14 01:32:55 +0500

    Update 2.5.1.cpp

--- 2.5.1.cpp
+++ 2.5.1.cpp
@@ -6,9 +6,9 @@ int n;
 int main()
 {
   cin >> n;
-  for (int i = 0; i < n; i++)
+  for (int i=0; i<n; i++)
   {
-    for (int j = 0; j <= i; j++)
+    for (int j=0; j<=i; j++)
     {
       cout << '*';
     }
