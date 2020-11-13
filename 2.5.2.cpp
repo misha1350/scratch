26d98329f5ffc45472a062fd5657d4cdacc46877
misha1350 <74230991+misha1350@users.noreply.github.com>
2020-11-14 01:33:43 +0500

    Update 2.5.2.cpp

--- 2.5.2.cpp
+++ 2.5.2.cpp
@@ -6,13 +6,13 @@ int n;
 int main()
 {
   cin >> n;
-  for (int i = 0; i < n; i++)
+  for (int i=0; i<n; i++)
   {
-    for (int j = i; j < n-1; j++)
+    for (int j=i; j<n-1; j++)
     {
       cout << ' ';
     }
-    for (int j = 0; j <= i; j++)
+    for (int j=0; j<=i; j++)
     {
       cout << '*';
     }
